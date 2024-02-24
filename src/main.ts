8e7f69d1d2d5ad963c634fd52d15f028cc1b1c91
wu985215993 <985215993@qq.com>
2024-02-25 03:25:53 +0800

    feat: test

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { WebsocketAPI } from '@binance/connector-typescript';
+// import { RestMarketTypes, Spot } from '@binance/connector-typescript';
 import * as dotenv from 'dotenv';
+import { Spot } from '@binance/connector';
 
 async function bootstrap() {
   dotenv.config();
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(8888);
 }
 bootstrap();
 
@@ -14,10 +15,30 @@ bootstrap();
 const API_KEY = process.env.API_KEY;
 const API_SECRET = process.env.API_SECRET;
 
-const callbacks = {
-  open: (client: WebsocketAPI) => client.exchangeInfo(),
-  close: () => console.debug('Disconnected from WebSocket server'),
-  message: (data: string) => console.info(JSON.parse(data)),
-};
-const websocketAPIClient = new WebsocketAPI(API_KEY, API_SECRET, { callbacks });
-setTimeout(() => websocketAPIClient.disconnect(), 20000);
+const BASE_URL = process.env.BASE_URL || 'https://api.binance.com/api/v3';
+
+// const client = new Spot(API_KEY, API_SECRET, {
+//   baseURL: 'https://api.binance.com/api/v3' || BASE_URL,
+// });
+
+// client
+//   .checkServerTime()
+//   .then((res: RestMarketTypes.checkServerTimeResponse) => {
+//     console.log(res);
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
+const client = new Spot(API_KEY, API_SECRET);
+// Get account information
+client.account().then((response) => client.logger.log(response.data));
+
+// Place a new order
+client
+  .newOrder('BNBUSDT', 'BUY', 'LIMIT', {
+    price: '350',
+    quantity: 1,
+    timeInForce: 'GTC',
+  })
+  .then((response) => client.logger.log(response.data))
+  .catch((error) => client.logger.error(error));
